5bff0c7c165732c9de387f205f9a7d9e39488d85
Richardson <richardson.andr@gmail.com>
2022-03-11 10:28:41 -0300

    commit

--- downloader.py
+++ downloader.py
@@ -1,6 +1,21 @@
+import requests
 from bs4 import BeautifulSoup as BS
 from pytube import YouTube, Playlist, Search
 
-s = Search('master of puppets')
+source = ('https://open.spotify.com/playlist/1z6axQOw8P27J7ysXtflql')
 
-print(s.results)
+site = requests.get(source).content
+
+soup = BS(site, 'html.parser')
+
+lista = soup.find_all(class_="eWYxOj")
+
+for i in lista:
+    print(i.string)
+
+# print(soup.prettify())
+
+# s = Search('Pump It')
+
+# stream = s.results[0].streams.get_by_itag(251)
+# stream.download()
