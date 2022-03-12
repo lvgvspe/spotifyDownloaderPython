c36de827db45401e1fc99325048cc909372d25c2
Lucas <lucas-camargo@outlook.com>
2022-03-12 02:42:29 -0300

    commit

--- downloader.py
+++ downloader.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as BS
 from pytube import Search
 import tkinter as tk
 from pydub import AudioSegment
+import os
 
 
 root = tk.Tk()
@@ -18,10 +19,13 @@ def downloader():
     lista = soup.find_all(class_="eWYxOj")
     for i in lista:
         s = Search(f"Music {i.string} {i.find_next('a').string}")
-        print(f'Downloading {s.results[0].title}')
+        print(f'Baixando {s.results[0].title}')
         stream = s.results[0].streams.get_by_itag(251)
-        stream.download(output_path=soup.find(class_='iJkkJW').string)
-        AudioSegment.from_file(f"{soup.find(class_='iJkkJW').string}/{s.results[0].title}").export(f"{soup.find(class_='iJkkJW').string}/{s.results[0].title}", format="mp3")
+        stream.download(output_path=f"{os.path.abspath(os.path.join(os.path.abspath(os.path.join('downloader.py', os.pardir)), os.pardir))}/{soup.find(class_='iJkkJW').string}")
+        print(f'Convertendo {stream.default_filename} para MP3')
+        AudioSegment.from_file(f"{os.path.abspath(os.path.join(os.path.abspath(os.path.join('downloader.py', os.pardir)), os.pardir))}/{soup.find(class_='iJkkJW').string}/{stream.default_filename}").export(f"{os.path.abspath(os.path.join(os.path.abspath(os.path.join('downloader.py', os.pardir)), os.pardir))}/{soup.find(class_='iJkkJW').string}/{stream.default_filename}.mp3", format="mp3")
+        os.remove(f"{os.path.abspath(os.path.join(os.path.abspath(os.path.join('downloader.py', os.pardir)), os.pardir))}/{soup.find(class_='iJkkJW').string}/{stream.default_filename}")
+        print(f'{s.results[0].title} - Salvo com sucesso')
     root.destroy()
 
 button = tk.Button(root, text="Baixar", command=downloader)
