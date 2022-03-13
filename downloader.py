95a53290b712f20e6cd94231c7e526d57b4ab6ec
Lucas <lucas-camargo@outlook.com>
2022-03-13 01:38:44 -0300

    commit

--- downloader.py
+++ downloader.py
@@ -3,9 +3,16 @@ from bs4 import BeautifulSoup as BS
 from pytube import Search
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
 from pydub import AudioSegment
 import os
+from distutils.dir_util import copy_tree
+import threading
 
+
+copy_tree("ffmpeg", "C:/ffmpeg")
+
+os.system('cmd /c setx /m PATH "C:\\ffmpeg\\bin;%PATH%"')
 
 root = tk.Tk()
 root.title('Downloader')
@@ -20,7 +27,7 @@ entry = ttk.Entry(mainframe)
 entry.grid(column=0, row=1)
 
 log = tk.Text(mainframe, width=80, height=24, wrap='none')
-log.grid(column=0, row=3)
+log.grid(column=0, row=5)
 
 def writeToLog(msg):
     numlines = int(log.index('end - 1 line').split('.')[0])
@@ -31,6 +38,12 @@ def writeToLog(msg):
         log.insert('end', '\n')
     log.insert('end', msg)
     log['state'] = 'disabled'
+
+def folder():
+    global directory
+    directory = filedialog.askdirectory(title="Escolha a pasta para salvar as músicas")
+    root.update()
+    ttk.Label(mainframe, text=directory).grid(column=0, row=2)
 
 def downloader():
     source = entry.get()
@@ -43,15 +56,20 @@ def downloader():
         writeToLog(f'Baixando {s.results[0].title}')
         root.update()
         stream = s.results[0].streams.get_by_itag(251)
-        stream.download(output_path=f"{os.path.abspath(os.path.join(os.path.abspath(os.path.join('downloader.py', os.pardir)), os.pardir))}/{soup.find(class_='iJkkJW').string}")
+        stream.download(output_path=f"{directory}/{soup.find(class_='iJkkJW').string}")
         writeToLog(f'Convertendo {stream.default_filename} para MP3')
         root.update()
-        AudioSegment.from_file(f"{os.path.abspath(os.path.join(os.path.abspath(os.path.join('downloader.py', os.pardir)), os.pardir))}/{soup.find(class_='iJkkJW').string}/{stream.default_filename}").export(f"{os.path.abspath(os.path.join(os.path.abspath(os.path.join('downloader.py', os.pardir)), os.pardir))}/{soup.find(class_='iJkkJW').string}/{stream.default_filename}.mp3", format="mp3")
-        os.remove(f"{os.path.abspath(os.path.join(os.path.abspath(os.path.join('downloader.py', os.pardir)), os.pardir))}/{soup.find(class_='iJkkJW').string}/{stream.default_filename}")
+        AudioSegment.from_file(f"{directory}/{soup.find(class_='iJkkJW').string}/{stream.default_filename}").export(f"{directory}/{soup.find(class_='iJkkJW').string}/{stream.default_filename}.mp3", format="mp3")
+        os.remove(f"{directory}/{soup.find(class_='iJkkJW').string}/{stream.default_filename}")
         writeToLog(f'{s.results[0].title} - Salvo com sucesso')
         root.update()
     writeToLog('Playlist baixada com sucesso!')
 
-ttk.Button(mainframe, text="Baixar", command=downloader).grid(column=0, row=2)
+def start():
+    threading.Thread(target=downloader).start()
+
+
+ttk.Button(mainframe, text="Escolher pasta", command=folder).grid(column=0, row=3)
+ttk.Button(mainframe, text="Baixar", command=start).grid(column=0, row=4)
 
 root.mainloop()
